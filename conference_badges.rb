2ea858032b166ba59404f194ad755477bcfcb40e
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-18 23:45:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,8 +10,8 @@ def batch_badge_creator(names)
   new_array
 end
 
-def assign_rooms(name, index)
-  index = [1,2,3,4,5,6,7]
+def assign_rooms(names, index)
+  newer_array = []
   room_numbers.each_with_index do |name, index|
     newer_array.push ("Hello,#{name}! You'll be assigned to room #{index}!")
   end
