36a20b8484907abbd36ba96e86ae9dac00da97b2
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-18 22:51:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,6 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.each do |name|
-    badge_message
-  end
-end
+def batch_badge_creator(names)
+  names.each do |name|
+    
