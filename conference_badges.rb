db51ab70de37e735633ccba13d473d1f80285e15
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-19 00:21:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,3 +17,13 @@ def assign_rooms(names)
   end
   newer_array
 end
+
+def printer(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(names).each do |assignment|
+    puts assignment
+  end
+end
