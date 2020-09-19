ff4b111c7e1802e7e20bd8317056b14e91d65538
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-19 00:04:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,8 @@ end
 
 def assign_rooms(names)
   newer_array = []
-  names.each_with_index do |name, index +1|
-    newer_array.push ("Hello, #{name}! You'll be assigned to room #{index}!")
+  names.each_with_index do |name, index|
+    newer_array.push ("Hello, #{name}! You'll be assigned to room #{index + 1}!")
   end
   newer_array
 end
